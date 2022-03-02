ab7ac68e4bbf6c4e380470db5c639a4d36bba9b0
Scott LaBelle <scott.labelle@snhu.edu>
2022-03-01 19:52:06 -0500

    Added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,53 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Profile {
-    _id: ID
-    name: String
-    email: String
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
     password: String
-    skills: [String]!
+    address: String
+    bookings: [Booking]!
+  }
+
+  type Service {
+    name: String!
+    description: String
+    length: String
+  }
+
+  type Booking {
+    _Id: ID!
+    user: User
+    time: String!
+    service: Service
+  }
+
+  input savedBookings {
+    _Id: ID!
+    user: User
+    desription: String
+    service: Service
   }
 
   type Auth {
     token: ID!
-    profile: Profile
+    user: User
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
+    services: [Service]
+    bookings: [Booking]
+    me: User
   }
 
   type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addUser(name: String!, email: String!, password: String!, address: String!): Auth
     login(email: String!, password: String!): Auth
-
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
+    addBooking(input: savedBookings!): User
+    deleteBooking()
+    removeUser: User
+   
   }
 `;
 
